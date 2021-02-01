d19aef06fd5eb1f6b8c8fb629d11dcbb204f14c8
Macdara O <macdara.dev@gmail.com>
2021-02-01 13:18:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+flip_a = b
+flip_b = a
+
+a = flip_a
+b = flip_b
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
